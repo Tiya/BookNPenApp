3c2942d60aeab1f422706a79b3374d3ec6cc194d
Tiya <tiyamartin@gmail.com>
2022-05-30 06:25:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularEpubViewerModule } from 'angular-epub-viewer';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AngularEpubViewerComponent } from './angular-epub-viewer/angular-epub-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AngularEpubViewerComponent
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularEpubViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
