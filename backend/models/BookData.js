b4b693696d3cd1af0ae5d5f074de7b309b0b0115
akshay98kv <akshaykv2k16@gmail.com>
2022-06-15 00:14:19 +0530

    bugfix: multiple db connection error

--- backend/models/BookData.js
+++ backend/models/BookData.js
@@ -4,15 +4,15 @@ const mongoose=require('mongoose');
 //Database connection
 // const db = "mongodb+srv://admin:1289lash@users.rs1bqhv.mongodb.net/?retryWrites=true&w=majority";
 //mongoose.connect('mongodb+srv://tiyamartin:Tiya.7256@tiyadatabase.bn7ry.mongodb.net/BookNPen?retryWrites=true&w=majority');
-mongoose.connect('mongodb+srv://FSDGroup3:Fsdgp3.123@cluster0.1f3izav.mongodb.net/BookNPen?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://FSDGroup3:Fsdgp3.123@cluster0.1f3izav.mongodb.net/BookNPen?retryWrites=true&w=majority');
 
-var conn = mongoose.connection;
-conn.on('connected', function() {
-    console.log('database is connected successfully');
-});
-conn.on('disconnected',function(){
-    console.log('database is disconnected successfully');
-})
+// var conn = mongoose.connection;
+// conn.on('connected', function() {
+//     console.log('database is connected successfully');
+// });
+// conn.on('disconnected',function(){
+//     console.log('database is disconnected successfully');
+// })
 //Schema definition
 const Schema= mongoose.Schema;
 
