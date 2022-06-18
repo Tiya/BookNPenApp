3813a9aee2ea302c01c59ac73a38254fa744e402
Abhilash Raju <abhilash.raju8@gmail.com>
2022-06-18 12:16:07 +0530

    Admin access

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
+let alert = require('alert'); 
+
+
 // const admin = require('../data/admin');
 
 // const db = "mongodb+srv://admin:1289lash@users.rs1bqhv.mongodb.net/?retryWrites=true&w=majority";
 
-const db = "mongodb+srv://FSDGroup3:Fsdgp3.123@cluster0.1f3izav.mongodb.net/?retryWrites=true&w=majority";
+const db = "mongodb+srv://FSDGroup3:Fsdgp3.123@cluster0.1f3izav.mongodb.net/BookNPen?retryWrites=true&w=majority";
 
 mongoose.connect(db, err=>{
     if(err){
@@ -32,21 +35,8 @@ router.post('/signup', (req,res)=>{
       });
 
       if((userData.username=='Admin')&&(userData.email)=='admin@domain.com'&&(userData.password)=='admin1234'){
-        userData.role = 'Admin'
-        let payload={subject:userData};
-        let token =jwt.sign(payload,'secretKey')
-        userData.save()
-        res.status(200).send({token});
-      }
-      else{
-         if(!!req.body.user){
-            userData.role="User";
-            let payload={subject:userData};
-            let token =jwt.sign(payload,'secretKey')
-            userData.save()
-            res.status(200).send({token});
-
-         }
+       alert("Hi! You can't Sign Up with Admin Credentials")
+        }
          else {
             userData.role = 'Author';
           // let user = new User(userData);
@@ -63,16 +53,40 @@ router.post('/signup', (req,res)=>{
           })
       }
     }
-})
+)
 
 router.post('/login',(req,res)=>{
-    let userData = req.body;
+    let userData = new User({
+        email: req.body.email,
+        password: req.body.password
+      });
 
-    // let UserData =  {
-    //     email:req.body.email,
-    //     password:req.body.password
-    // };
-    
+      if(userData.email=="admin@domain.com" && userData.password=="admin1234")
+      {
+        User.findOne({email : userData.email},(error,user)=>
+        {
+            if(error)
+            {
+                console.log(error);
+            }
+            else
+            if(!user.email)
+            {
+            userData.username="Admin";
+            userData.role="Admin";
+            let payload={subject:userData};
+            let token =jwt.sign(payload,'secretKey')
+            userData.save()
+            res.status(200).send({token});
+            }
+            else{
+            let payload={subject:user};
+            let token =jwt.sign(payload,'secretKey')
+            res.status(200).send({token});
+            }
+        })
+        }
+        else{
     User.findOne({email : userData.email},(error,user)=>
     {
         if(error)
@@ -97,17 +111,7 @@ router.post('/login',(req,res)=>{
             }
         }
     })
-
-    // for(let i=0;i<admin.length;i++){
-    //     if(UserData.email==admin[i].email && UserData.password==admin[i].password)
-    //                 {
-    //                 let payload={subject:admin};
-    //                 let token =jwt.sign(payload,'secretKey')
-    //                 res.status(200).send({token});
-    //                 }
-    // }
-})
-
-
+      }}
+)
 
 module.exports = router;
